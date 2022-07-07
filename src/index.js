b98c90d886cb51f74acde0ad3b2741bcb559fad0
MK <ma_zhengke@sina.com>
2022-07-07 12:10:54 +0800

    02

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
 import _ from 'lodash';
+import './style.css'
+import Icon from './C.png'
+
+import printMe from "./print";
 
 function component() {
     var element = document.createElement('div');
 
-    // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    var btn = document.createElement('button');
+    btn.innerHTML = 'click';
+    btn.onclick = printMe;
 
+
+    // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
+    element.innerHTML = _.join(['Hello', 'webpackab'], ' ');
+    element.classList.add('hello');
+
+    var myIcon = new Image();
+    myIcon.src = Icon;
+
+    // element.appendChild(myIcon);
+
+    element.appendChild(btn);
     return element;
 }
 
